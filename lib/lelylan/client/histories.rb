d89e054fb88a9c75e65371b8a63fa3ade870e56d
Andrea Reginato <andrea.reginato@gmail.com>
2013-01-24 20:34:19 +0100

    Refactoring histories

--- lib/lelylan/client/histories.rb
+++ lib/lelylan/client/histories.rb
@@ -2,40 +2,26 @@ module Lelylan
   class Client
     module Histories
 
-      # Public: Returns extended information for a given history identified from its URI.
-      # Find more at {http://dev.lelylan.com/rest/devices/history/#get Lelylan Dev Center}.
       #
-      # history - A String that represents the history URI.
+      # Public: Returns extended information for a given history identified from its ID.
       #
-      # Returns Hashie The history resource.
+      # id - A String that represent the history ID.
       #
-      # Examples
+      # Returns Hashie The history.
       #
-      #   history = "http://api.lelylan.com/histories/4dcb9e23d033a9088900200f"
-      #   client.history(history)
-      #
-      def history(history)
-        get("/histories/#{find_id(history)}")
+      def find_history(id)
+        get("/histories/#{id}")
       end
 
-      # Public: Returns a list of history resources for a given device identified from its URI.
-      # Find more at {http://dev.lelylan.com/rest/devices/history/#all Lelylan Dev Center}.
       #
-      # device - A String that represents the device URI.
-      # options - The Hash option used to refine the search (default: {}). 
-      #           Check out the {http://dev.lelylan.com/rest/devices/history/#all API doc} for the accepted options.
-      # 
-      # Returns Hashie List of histories.
+      # Public: Returns a list of histories related to owned devices.
       #
-      # Examples
-      # 
-      #   # Returns the first 10 histories for a specific device)
-      #   device = "http://api.lelyla.com/devices/4dcb9e23d033a9088900023b"
-      #   client.histories(device, per: 10)
+      # params - The Hash used to refine the search (default: {}).
       #
+      # Returns Array List of histories.
       #
-      def histories(device, options = {})
-        get("/devices/#{find_id(device)}/histories", options)
+      def all_histories(params = {})
+        get('/histories', params)
       end
     end
   end
--- spec/lelylan/client/histories_spec.rb
+++ spec/lelylan/client/histories_spec.rb
@@ -2,63 +2,47 @@ require 'helper'
 
 describe Lelylan::Client::Histories do
 
-  let(:client) do
+  let(:lelylan) do
     a_client
   end
 
 
-  describe '.history' do
-
-    let(:path) do
-      '/histories/4dcb9e23d033a9088900000e'
-    end
-
-    let(:uri) do
-      "http://api.lelylan.com/#{path}"
-    end
+  describe '#find_history' do
 
     before do
-      stub_get(path).to_return(body: fixture('history.json'))
+      stub_get('/histories/1').to_return(body: fixture('history.json'))
     end
 
     let!(:history) do
-      client.history(uri)
+      lelylan.find_history('1')
     end
 
     it 'returns the history' do
-      history.uri.should_not be_nil
+      history.id.should_not be_nil
     end
 
     it 'sends the request' do
-      a_get(path).should have_been_made
+      a_get('/histories/1').should have_been_made
     end
   end
 
 
-  describe '.histories' do
-
-    let(:path) do
-      '/devices/4dcb9e23d033a9088900000a'
-    end
-
-    let(:uri) do
-      "http://api.lelylan.com/#{path}"
-    end
+  describe '#all_histories' do
 
     before do
-      stub_get("#{path}/histories").with(query: {per: 10}).to_return(body: fixture('histories.json'))
+      stub_get('/histories').with(query: {per: 10}).to_return(body: fixture('histories.json'))
     end
 
     let!(:histories) do
-      client.histories(uri, per: 10)
+      lelylan.all_histories(per: 10)
     end
 
     it 'returns the histories' do
-      histories.first.uri.should_not be_nil
+      histories.first.id.should_not be_nil
     end
 
     it 'sends the request' do
-      a_get("#{path}/histories").with(query: {per: 10}).should have_been_made
+      a_get('/histories').with(query: {per: 10}).should have_been_made
     end
   end
 end
